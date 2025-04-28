5f116b5ced7880db33c6e19d91119895bd6e21bb
Elena <Elena3405674@mail.ru>
2025-04-28 20:25:58 +0300

    add: create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import fs from "fs/promises";
+import { dirname } from "path";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const getFilenameFromUrl = fileURLToPath;
+const getDirnameFromUrl = (url) => dirname(fileURLToPath(url));
+
+const _dirname = getDirnameFromUrl(import.meta.url);
+const pathToFile = path.join(_dirname, "files", "fresh.txt");
+
 const create = async () => {
-    // Write your code here 
+    try {
+        await fs.writeFile(pathToFile, "I am fresh and young", { flag: "wx" })
+    } catch {
+        throw new Error("FS operation failed");
+    }
 };
 
 await create();
