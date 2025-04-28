facbc0ee9d7a945d7773fb9a1db67b82667226f0
Elena <Elena3405674@mail.ru>
2025-04-29 01:45:14 +0300

    add: hash

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -4,9 +4,9 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const getDirnameFromUrl = (url) => dirname(fileURLToPath(url));
-const _dirname = getDirnameFromUrl(import.meta.url);
-const sourceDir = path.join(_dirname, "files");
-const targetDir = path.join(_dirname, "files_copy");
+const __dirname = getDirnameFromUrl(import.meta.url);
+const sourceDir = path.join(__dirname, "files");
+const targetDir = path.join(__dirname, "files_copy");
 
 const copy = async () => {
     try{
